a26684745a28b0d84be8e2356c95836dd82f0690
ElizaGFranco <65580249+ElizaGFranco@users.noreply.github.com>
2020-05-30 21:01:05 -0700

    Add files via upload

---
+++ Cine.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int opcion;
+	char temp[10];
+   
+   do{
+        printf("Bienvenidos a CineEnCasa\n");
+        printf("1.- Clasificacion de la pelicula\n");
+        printf("2.- Seleccionar la pelicula\n");
+        printf("3.- Consultar horarios\n");
+        printf("4.- Disponibilidad de sala\n"); 
+		printf("5.- Comprar boletos\n");
+        printf("6.- Descuentos\n");
+        printf("7.- Dulceria\n");
+        printf("8.- Imprimir ticket\n");
+        printf("9.- Compra-Renta peliculas\n");
+        printf("10.- Reistrarse\n");
+        printf("11.- Salir \n");
+        fgets(temp= atoi(temp);
+        opcion = atoi(temp);
+        //Leer
+        
+        switch(opcion){
+        	case 1;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+            case 2;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 3;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 4;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 5;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 6;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 7;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 8;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 9;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 10;
+        	printf("Usted escogio la clasificacion de la pelicula\n");
+        	break;
+        	case 11;
+        	break;
+        	default;
+        	    printf("Opcion");
+        	    break;
+        }
+    } while (opcion != 11);
+
+    return 0;
+}
+
+
+
